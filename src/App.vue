f15252af9532a2a2c5c69e71f1d46c0044545be1
lifuhai1106 <1109372147@qq.com>
2020-12-31 15:40:02 +0800

    优化

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,16 @@
 <template>
   <div id="app">
-    <!-- <div>
-      <tagtree
-        title="栏目选择"
-        width="300"
-        :dataList="columnList"
-        :props="defaultProps"
-        nodeKey="subjectUuid"
-      >
-      </tagtree>
-    </div> -->
   </div>
 </template>
 
 <script>
-// import tagtree from "./packages/tagtree";
 
 export default {
   name: "App",
   components: {
-    // tagtree,
   },
   data() {
     return {
-      defaultProps: {
-        label: "name",
-        children: "subjectList",
-      },
-      columnList: [
-        {
-          name: "栏目1",
-          subjectUuid: "1",
-          subjectList: [
-            {
-              name: "栏目1-1",
-              subjectUuid: "1-1",
-            },
-          ],
-        },
-        {
-          name: "栏目2",
-          subjectUuid: "2",
-          subjectList: [],
-        },
-      ],
     };
   },
 };
